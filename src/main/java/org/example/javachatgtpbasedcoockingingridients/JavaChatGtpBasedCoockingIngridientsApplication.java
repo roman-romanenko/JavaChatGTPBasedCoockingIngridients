eb750fb18128f7a5f641a1c810265cdf856229e0
Roman Romanenko <capital.car7@gmail.com>
2026-01-14 18:17:25 +0100

    Initial commit

---
+++ src/main/java/org/example/javachatgtpbasedcoockingingridients/JavaChatGtpBasedCoockingIngridientsApplication.java
@@ -0,0 +1,13 @@
+package org.example.javachatgtpbasedcoockingingridients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaChatGtpBasedCoockingIngridientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaChatGtpBasedCoockingIngridientsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/javachatgtpbasedcoockingingridients/JavaChatGtpBasedCoockingIngridientsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javachatgtpbasedcoockingingridients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaChatGtpBasedCoockingIngridientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
